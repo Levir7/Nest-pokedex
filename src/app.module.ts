52a061713edc908746625826b49394d3d63b5c33
Levir Olea <levirolear@outlook.com>
2024-03-18 23:47:55 -0600

    version 1 ready with conection db MONGO

--- src/app.module.ts
+++ src/app.module.ts
@@ -21,7 +21,9 @@ import { JoiValidationSchema } from './config/joi.validation';
       rootPath: join(__dirname, '..', 'public'),
     }),
 
-    MongooseModule.forRoot(process.env.MONGODB),
+    MongooseModule.forRoot(process.env.MONGODB, {
+      dbName: 'pokemonsDB'
+    }),
 
     PokemonModule,
 
